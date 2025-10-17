b8321a5666ce32db2d22966f25ab9ad9626bf307
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-17 17:28:52 +0500

    Task0

--- Tyuiu.DanilovAS.Sprint5.Task0.V6.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint5.Task0.V6.Lib/DataService.cs
@@ -1,11 +1,12 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Diagnostics;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.DanilovAS.Sprint5.Task0.V6.Lib
 {
     public class DataService : ISprint5Task0V6
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine("C:", "Users" , "shura" , "source" , "repos" , "Tyuiu.DanilovAS.Sprint5" , "Tyuiu.DanilovAS.Sprint5.Task0.V6" , "bin" , "Debug" , "net8.0" , "OutPutFileTask0.txt");
 
             double z = x / (Math.Sqrt(Math.Pow(x, 2) + x));
             z = Math.Round(z, 3);
