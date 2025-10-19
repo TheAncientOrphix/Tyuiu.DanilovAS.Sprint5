53d13efc804516c0729267ab43e542581b9f7d5f
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-19 10:12:22 +0500

    Task2

---
+++ Tyuiu.DanilovAS.Sprint5.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.DanilovAS.Sprint5.Task2.V11.Lib;
+namespace Tyuiu.DanilovAS.Sprint5.Task2.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+            string path = $@"C:\Users\shura\AppData\Local\Temp\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool fileExists = File.Exists(path);
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint5.Task2.V11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+using Tyuiu.DanilovAS.Sprint5.Task2.V11.Lib;
+namespace Tyuiu.DanilovAS.Sprint5.Task2.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл.     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить нечетные элементы массива на 0.       *");
+            Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+
+            int[,] matrix = new int[3, 3] {{ 4, 9, 3 },{ 5, 8, 8},{ 5, 7, 5} };
+
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            
+            for(int i = 0; i<rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string result = ds.SaveToFileTextData(matrix);
+            Console.WriteLine($"Файл {result} создан!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
