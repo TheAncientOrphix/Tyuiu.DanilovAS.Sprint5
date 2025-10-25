1ef22e1f33b40e43b536ca62768f4650ee0e53b2
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-25 10:00:15 +0500

    Task6

---
+++ Tyuiu.DanilovAS.Sprint5.Task6.V25.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.DanilovAS.Sprint5.Task6.V25.Lib;
+namespace Tyuiu.DanilovAS.Sprint5.Task6.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheck()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\DataSprint6\InPutDataFileTask6V25.txt";
+
+            int wait = 6;
+            var res = ds.LoadFromDataFile(path);
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint6\InPutDataFileTask6V25.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint5.Task6.V25/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.DanilovAS.Sprint5.Task6.V25.Lib;
+namespace Tyuiu.DanilovAS.Sprint5.Task6.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти количество знаков восклицания в заданной строке.                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint6\InPutDataFileTask6V25.txt";
+            Console.WriteLine($"Данные находятся в файле: {path}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.LoadFromDataFile(path);
+            Console.WriteLine($"Ответ равен => {res}");
+            Console.ReadKey();
+        }
+    }
+}
