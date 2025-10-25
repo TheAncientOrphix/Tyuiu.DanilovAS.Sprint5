c866332fa023f30dcdf7c825645f66af2528ac10
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-25 09:02:55 +0500

    Task4

---
+++ Tyuiu.DanilovAS.Sprint5.Task4.V5.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.DanilovAS.Sprint5.Task4.V5.Lib
+{
+    public class DataService : ISprint5Task4V5
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            strX = strX.Replace('.', ',');
+
+            double x = Convert.ToDouble(strX);
+            double y = (4.26 * x )/ (Math.Sin(x));
+
+            return Math.Round(y,3);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint5.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.DanilovAS.Sprint5.Task4.V5.Lib;
+namespace Tyuiu.DanilovAS.Sprint5.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint4\InPutDataFileTask4V5.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait,fileExists);
+        }
+    }
+}
